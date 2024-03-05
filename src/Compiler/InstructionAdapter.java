18a1af44d68d3a473eb5eecb56f6ad817de1efbf
Abdulrahman Alali <22000577@ug.buid.ac.ae>
2024-03-05 22:27:30 +0400

    I-type, R-type adapters

---
+++ src/Compiler/InstructionAdapter.java
@@ -0,0 +1,21 @@
+package Compiler;
+
+import java.nio.ByteBuffer;
+
+public class InstructionAdapter {
+
+}
+
+//public class Instruction {
+//    private byte[] opCode;
+//
+//    public byte[] getOpCode() {
+//        return opCode;
+//    }
+//
+//    public int getOpcode() {
+//        ByteBuffer buffer = ByteBuffer.wrap(opCode);
+//        return buffer.getInt();
+//    }
+//}
+
---
+++ src/Compiler/MipsIntruction.java
@@ -0,0 +1,2 @@
+package Compiler;public interface MipsIntruction {
+}
