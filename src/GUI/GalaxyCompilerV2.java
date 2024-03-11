500aacf74a3b5e152ec48183b0e77ec638bdd96e
Afzal <117006180+Xynoxian@users.noreply.github.com>
2024-03-11 23:54:17 +0400

    Cool splash screen, you can change the design text and image via the initComponents

--- src/GUI/GalaxyCompilerV2.java
+++ src/GUI/GalaxyCompilerV2.java
@@ -3,12 +3,13 @@ package GUI;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 
 import Compiler.Addresses.AddressProvider;
 import Compiler.Register.Register;
 import Compiler.Register.RegisterProvider;
-
 
 public class GalaxyCompilerV2 extends JFrame {
 
@@ -18,8 +19,6 @@ public class GalaxyCompilerV2 extends JFrame {
     private JTable registerTable;
     private AddressProvider addressProvider;
     private RegisterProvider registerProvider;
-
-    public JButton executeButton;
 
     public GalaxyCompilerV2(AddressProvider addressProvider, RegisterProvider registerProvider) {
         this.addressProvider = new AddressProvider();
@@ -41,13 +40,12 @@ public class GalaxyCompilerV2 extends JFrame {
         editorPanel.add(editPanelScroll);
 
         //Register value panel to enter the register values etc
-        registerPanel=new JTextArea();
+        registerPanel = new JTextArea();
         registerPanel.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 15));
-        JScrollPane registerPanelScroll= new JScrollPane(registerPanel);
+        JScrollPane registerPanelScroll = new JScrollPane(registerPanel);
         editorPanel.add(registerPanelScroll);
 
-        mainPanel.add(editorPanel,BorderLayout.CENTER);
-
+        mainPanel.add(editorPanel, BorderLayout.CENTER);
 
         // Register table displaying registers, their numbers and values
         String[] columnNames = {"Index", "Register", "Value"};
@@ -58,7 +56,6 @@ public class GalaxyCompilerV2 extends JFrame {
         registerTable = new JTable(data, columnNames);
         JScrollPane registerTableScroll = new JScrollPane(registerTable);
         mainPanel.add(registerTableScroll, BorderLayout.EAST);
-
 
         // Console output for the .asm code
         outputConsole = new JTextArea(10, 40); // Increase dimensions
@@ -76,7 +73,7 @@ public class GalaxyCompilerV2 extends JFrame {
         fileMenu.add(saveItem);
         menuBar.add(fileMenu);
 
-        executeButton = new JButton("Execute");
+        JButton executeButton = new JButton("Execute");
         executeButton.addActionListener(e -> executeCode());
 
         JButton clearOutputButton = new JButton("Clear Output");
@@ -98,10 +95,71 @@ public class GalaxyCompilerV2 extends JFrame {
         saveItem.addActionListener(e -> saveFile());
     }
 
+    public static class PopupWindow extends JWindow {
+        private JProgressBar progressBar;
+        private JLabel label;
+        private Timer timer;
+        private int progress = 0;
+        private GalaxyCompilerV2 GUI;
+
+        public PopupWindow(GalaxyCompilerV2 GUI) {
+            this.GUI = GUI;
+            setSize(400, 200);
+            setLocationRelativeTo(null);
+            setVisible(true);
+            initComponents();
+            startTimer();
+        }
+
+        private void initComponents() {
+            //Panel to hold the stuff
+            JPanel popupPanel = new JPanel(new BorderLayout());
+
+            label = new JLabel("Loading GALAXY V.0.0.1......", SwingConstants.CENTER);
+            label.setFont(new Font("Arial", Font.BOLD, 18)); // Change the font and size here
+            label.setForeground(Color.BLUE); // Change the font color here
+            popupPanel.add(label, BorderLayout.NORTH);
+
+            //Le Progress Bar
+            progressBar = new JProgressBar();
+            progressBar.setStringPainted(true);
+            popupPanel.add(progressBar, BorderLayout.CENTER);
+
+            ImageIcon imageIcon = new ImageIcon("C:\\Users\\Afzal\\Downloads\\MIPS\\Images\\galaxy-stars-space-digital-art-4k-wallpaper-pixground.jpg");
+            Image image = imageIcon.getImage().getScaledInstance(400, 200, Image.SCALE_SMOOTH); // Scale the image to fit the popup window dimensions
+            JLabel imageLabel = new JLabel(new ImageIcon(image));
+            popupPanel.add(imageLabel, BorderLayout.SOUTH);
+
+            setContentPane(popupPanel);
+        }
+
+        private void startTimer() {
+            timer = new Timer(100, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    progress += 10;
+                    progressBar.setValue(progress);
+                    label.setText("Loading GALAXY..." + progress + "%");
+                    if (progress >= 100) {
+                        timer.stop();
+                        dispose();
+                        showMainGUI();
+                    }
+                }
+            });
+
+            timer.start();
+        }
+
+        private void showMainGUI() {
+            GUI.setVisible(true);
+        }
+    }
+
     public void refreshDataTable(Object[][] data) {
         Register[] registerArray = registerProvider.getRegisterArray();
 
-        for(int i = 0; i < registerArray.length; i+=4) {
+        for (int i = 0; i < registerArray.length; i += 4) {
             data[i / 4][0] = i / 4;
             data[i / 4][1] = registerArray[i].getRegisterHumanName();
             data[i / 4][2] = registerArray[i].getValue();
@@ -109,8 +167,15 @@ public class GalaxyCompilerV2 extends JFrame {
         }
     }
 
+    public static void main(String[] args) {
+        AddressProvider addressProvider = new AddressProvider();
+        RegisterProvider registerProvider = new RegisterProvider(addressProvider);
+        GalaxyCompilerV2 GUI = new GalaxyCompilerV2(addressProvider, registerProvider);
 
-
+        SwingUtilities.invokeLater(() -> {
+            new PopupWindow(GUI);
+        });
+    }
 
     //Function to open a .asm file
     private void openFile() {
@@ -136,7 +201,6 @@ public class GalaxyCompilerV2 extends JFrame {
         }
     }
 
-
     //Function to save file to .asm
     private void saveFile() {
         JFileChooser fileChooser = new JFileChooser();
@@ -159,47 +223,40 @@ public class GalaxyCompilerV2 extends JFrame {
         }
     }
 
-    public String[] executeCode() {
-
+    private void executeCode() {
         String code = editPanel.getText();
-        int numberOfCommands=editPanel.getLineCount();
+        try {
+            Process process = Runtime.getRuntime().exec("mips-compiler");
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 
-            String[] instructionMemory=code.split("\n");
+            // Write code to process's input stream
+            process.getOutputStream().write(code.getBytes());
+            process.getOutputStream().flush();
 
-            return instructionMemory;
+            // Read output and error streams
+            String line;
+            StringBuilder output = new StringBuilder();
+            StringBuilder error = new StringBuilder();
+            while ((line = inputReader.readLine()) != null) {
+                output.append(line).append("\n");
+                updateRegisterTable(line);
+            }
+            while ((line = errorReader.readLine()) != null) {
+                error.append(line).append("\n");
+            }
 
-
-
-
-//            Process process = Runtime.getRuntime().exec("mips-compiler");
-//            BufferedReader inputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-//            BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-//
-//            // Write code to process's input stream
-//            process.getOutputStream().write(code.getBytes());
-//            process.getOutputStream().flush();
-//
-//            // Read output and error streams
-//            String line;
-//            StringBuilder output = new StringBuilder();
-//            StringBuilder error = new StringBuilder();
-//            while ((line = inputReader.readLine()) != null) {
-//                output.append(line).append("\n");
-//                updateRegisterTable(line);
-//            }
-//            while ((line = errorReader.readLine()) != null) {
-//                error.append(line).append("\n");
-//            }
-//
-//            // Update console output
-//            outputConsole.setText(output.toString());
+            // Update console output
+            outputConsole.setText(output.toString());
 
             // Wait for the process to complete
-//            int exitCode = process.waitFor();
-//            if (exitCode != 0) {
-//                outputConsole.append("Execution failed with exit code " + exitCode);
-//            }
-
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                outputConsole.append("Execution failed with exit code " + exitCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private void updateRegisterTable(String line) {
@@ -217,5 +274,4 @@ public class GalaxyCompilerV2 extends JFrame {
             }
         }
     }
-
 }
